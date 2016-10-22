b6b82f93645ee9864e4166b2c205fc999da9fc09
Cowan <ctcumming@gmail.com>
2016-10-22 12:45:48 -0700

    Added failed_guess predicate for users to input.

--- clueHelper.pl
+++ clueHelper.pl
@@ -1,6 +1,9 @@
-:- dynamic person/1, weapon/1, room/1.
+:- dynamic person/1, weapon/1, room/1, cdef/0, abcd/0.
+
+ourList([],5).
 
 
+%% possible suspects
 person(colonel_mustard).
 person(mrs_peacock).
 person(mrs_scarlet).
@@ -8,6 +11,7 @@ person(professor_plum).
 person(mr_green).
 person(mrs_white).
 
+%% possible murder weapons used
 weapon(knife).
 weapon(candlestick).
 weapon(revolver).
@@ -15,9 +19,10 @@ weapon(rope).
 weapon(lead_pipe).
 weapon(wrench). 
 
+%% possible rooms 
 room(hall).
 room(lounge).
-room(dining_room).
+room(dining_room). 
 room(kitchen). 
 room(ballroom). 
 room(conservatory). 
@@ -25,8 +30,26 @@ room(billiard_room).
 room(library).
 room(study). 
 
+abcd :- assert(cdef).
+
+possible(P, W, R) :- person(P), weapon(W), room(R), \+ failedGuess(P, W, R). 
 
 
-possible(P, W, R) :- person(P), weapon(W), room(R). 
+%% possibilities(P, W, R, Chance) is true if the chance of P W and R being the answer
+%% is at least Chance
 
 
+%% chance(P, W, R, Chance) is true if person P, weapon W, and Room R have Chance chance
+%% of being the correct answer
+
+
+%% highest_chance(P, W, R) is true if person P, weapon W, and room R have the highest
+%% chance of being the correct answer
+
+
+
+%% failed_guess(P, W, R) is true if person P, weapon W, and room R have been guessed 
+%% and proven wrong 
+
+failed_guess(colonel_mustard, knife, hall). 
+failed_guess(mrs_scarlet, wrench, hall). 

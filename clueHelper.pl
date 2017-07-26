2bbe0a3e1dbabd7d461ce85eed62144c8038ec4a
trinityalps <stansbury.er@gmail.com>
2017-07-26 14:59:04 -0700

    Update clueHelper.pl

--- clueHelper.pl
+++ clueHelper.pl
@@ -1,7 +1,9 @@
-:- dynamic person/1, weapon/1, room/1, cdef/0, abcd/0.
+:- dynamic person/1, weapon/1, room/1, failed_guess/3, current_people/1, 
+	current_weapons/1, current_rooms/1.
 
-ourList([],5).
-
+current_people([colonel_mustardk,mrs_peacock, mrs_scarlet, professor_plum, mr_green, mrs_white]).
+current_weapons([knife, candlestick, revolver, rope, lead_pipe, wrench]). 
+current_rooms([hall, lounge, dining_room, kitchen, ballroom, conservatory, billiard_room, library, study]).
 
 %% possible suspects
 person(colonel_mustard).
@@ -30,10 +32,45 @@ room(billiard_room).
 room(library).
 room(study). 
 
-abcd :- assert(cdef).
 
-possible(P, W, R) :- person(P), weapon(W), room(R), \+ failedGuess(P, W, R). 
+%% people(L) is true if L is the list of all possible suspects
+people(L) :- findall(X0, person(X0), L). 
 
+%% weapons(L) is true if L is the list of all possible murder weapons
+weapons(L) :- findall(X0, weapon(X0), L). 
+
+%% rooms(L) is true if L is the list of all possible rooms
+rooms(L) :- findall(X0, room(X0), L).
+
+%% possible(P, W, R) is true if person P, weapon W and room R could make a 
+%% possible answer
+possible(P, W, R) :- person(P), weapon(W), room(R), \+ failed_guess(P, W, R). 
+
+%% does the same as possible but organized into a triple
+possible(a(P, W, R)) :- possible(P, W, R). 
+
+%% all_possibilities(L) is true when L is the list of all possible answers
+all_possibilities(L) :- findall(X0, possible(X0), L).
+
+%% total_possibilities(X) is true if X is the total number of possible combinations
+%% that the answer could be
+total_possibilities(X) :- all_possibilities(L), length(L, X). 
+
+%% chance(X) is true if X is the percentage chance of any one of the answers left
+%% to be true
+chance(X) :- total_possibilities(Y), X is 100 / Y.
+
+%% remove_person(X) retracts current_people(L), and then asserts current_people(L2)
+%% where L2 is L without X in it 
+remove_person(X) :- current_people(L), delete(L, X, L2), retract(current_people(L)), assert(current_people(L2)). 
+
+%% remove_weapon(X) retracts current_weapons(L), and then asserts current_weapons(L2)
+%% where L2 is L without X in it
+remove_weapon(X) :- current_weapons(L), delete(L, X, L2), retract(current_weapons(L)), assert(current_weapons(L2)). 
+
+%% remove_room(X) retracts current_rooms(L), adn then asserts current_rooms(L2)
+%% where L2 is L without X in it
+remove_room(X) :- current_rooms(L), delete(L, X, L2), retract(current_rooms(L)), assert(current_rooms(L2)). 
 
 %% possibilities(P, W, R, Chance) is true if the chance of P W and R being the answer
 %% is at least Chance
@@ -48,8 +85,7 @@ possible(P, W, R) :- person(P), weapon(W), room(R), \+ failedGuess(P, W, R).
 
 
 
-%% failed_guess(P, W, R) is true if person P, weapon W, and room R have been guessed 
-%% and proven wrong 
 
-failed_guess(colonel_mustard, knife, hall). 
-failed_guess(mrs_scarlet, wrench, hall). 
+%% failed_guess(P, W, R) is true if person P, weapon W, and room R have been guessed 
+%% and proven wrong i.e. one or more of the 3 is not the correct answer
+
